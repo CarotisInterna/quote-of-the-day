f4ca1dd92ff4ccf22e85499f8c94e2446bca2fbe
e.popova <interna.carotis@gmail.com>
2024-09-07 18:53:33 +0200

    initial commit

---
+++ src/main/java/com/example/quote_of_the_day/QuoteOfTheDayApplication.java
@@ -0,0 +1,13 @@
+package com.example.quote_of_the_day;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuoteOfTheDayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuoteOfTheDayApplication.class, args);
+	}
+
+}
